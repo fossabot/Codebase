bd7f2840cfca12c52c0724751efb1df8e7843727
Vincent Thomas <vincent.nathan.thomas@gmail.com>
2022-02-20 19:58:30 +0100

    testing workflows

--- libs/movie/feature-public/jest.config.js
+++ libs/movie/feature-public/jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  displayName: 'movie-feature-public',
-  preset: '../../../jest.preset.js',
+  displayName: "movie-feature-public",
+  preset: "../../../jest.preset.js",
   transform: {
-    '^.+\\.[tj]sx?$': ['@swc/jest', { jsc: { transform: { react: { runtime: 'automatic' } } } }]
+    "^.+\\.[tj]sx?$": [
+      "@swc/jest",
+      { jsc: { transform: { react: { runtime: "automatic" } } } },
+    ],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/movie/feature-public'
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../../coverage/libs/movie/feature-public",
 };
